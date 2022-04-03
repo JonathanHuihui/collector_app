b69b8f9d63be4838b2896c1dcb83287f3bce5449
Jonathan Huihui <30911328+JonathanHuihui@users.noreply.github.com>
2022-04-03 15:10:00 -0400

    separate sql scripts

--- postgres/pgconf/table.sql
+++ postgres/pgconf/table.sql
@@ -1,11 +1,4 @@
-
-
-CREATE TABLE IF NOT EXISTS public.users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username TEXT,
-    pswd TEXT,
-);
-
-CREATE INDEX idx_event_aggregate_id ON public.users (username);
-
-COMMIT;
+    pswd TEXT);
