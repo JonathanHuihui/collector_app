3b6195aac2027b2ac1276f7c248486c696ebb4ed
Jonathan Huihui <30911328+JonathanHuihui@users.noreply.github.com>
2022-04-03 15:10:22 -0400

    insert dummy data, separate script

---
+++ postgres/pgconf/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO users (username, pswd) VALUES ('jonathan', 'password');
+
+INSERT INTO users (username, pswd) VALUES ('crunchy', 'data');
+
+/*CREATE INDEX idx_event_aggregate_id ON users (username);*/
